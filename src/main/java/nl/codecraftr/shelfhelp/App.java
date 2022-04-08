54f04ac897ec47d39327dd8ea55aad97916c4104
Roy <roy@codecraftr.nl>
2022-04-08 16:28:54 +0200

    Added checkstyle for automated code formatting checks

--- src/main/java/nl/codecraftr/shelfhelp/App.java
+++ src/main/java/nl/codecraftr/shelfhelp/App.java
@@ -1,10 +1,11 @@
 package nl.codecraftr.shelfhelp;
 
 /**
- * Hello world!
+ * Hello world.
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
