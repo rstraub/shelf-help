3d7afa9df3c7bf66a6c3255d0e52c2b1eb58d9b6
Roy <roy@codecraftr.nl>
2022-04-10 21:32:16 +0200

    Failing integration test for reading csv file of books

---
+++ src/main/java/nl/codecraftr/shelfhelp/csv/CsvRepository.java
@@ -0,0 +1,15 @@
+package nl.codecraftr.shelfhelp.csv;
+
+import java.util.Collections;
+import java.util.Set;
+import nl.codecraftr.shelfhelp.domain.model.Book;
+
+/**
+ * Book repository for csv files.
+ */
+public class CsvRepository {
+
+  public Set<Book> getAll() {
+    return Collections.emptySet();
+  }
+}
--- src/test/java/nl/codecraftr/shelfhelp/csv/CsvBookRepositoryIT.java
+++ src/test/java/nl/codecraftr/shelfhelp/csv/CsvBookRepositoryIT.java
@@ -1,23 +1,33 @@
 package nl.codecraftr.shelfhelp.csv;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
 import nl.codecraftr.shelfhelp.domain.model.Book;
 import nl.codecraftr.shelfhelp.domain.model.Effort;
 import nl.codecraftr.shelfhelp.domain.model.Payoff;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+class CsvBookRepositoryIT {
 
-import static org.assertj.core.api.Assertions.assertThat;
+  private CsvRepository repository;
 
-public class CsvBookRepositoryIT {
-    @Test
-    void shouldReturnBooksGivenCsvEntries() {
-        var expected = List.of(
-                new Book("Pragmatic Programmer", Effort.LOW, Payoff.MEDIUM),
-                new Book("Programmers Brain", Effort.VERY_LOW, Payoff.MEDIUM),
-                new Book("Domain Driven Design", Effort.HIGH, Payoff.VERY_HIGH)
-        );
+  @BeforeEach
+  void setUp() {
+    repository = new CsvRepository();
+  }
 
-        assertThat(false).isFalse();
-    }
+  @Test
+  void shouldReturnBooksGivenCsvEntries() {
+    var expected = Set.of(
+        new Book("Pragmatic Programmer", Effort.LOW, Payoff.MEDIUM),
+        new Book("Programmers Brain", Effort.VERY_LOW, Payoff.MEDIUM),
+        new Book("Domain Driven Design", Effort.HIGH, Payoff.VERY_HIGH)
+    );
+
+    var result = repository.getAll();
+
+    assertThat(result).isEqualTo(expected);
+  }
 }
