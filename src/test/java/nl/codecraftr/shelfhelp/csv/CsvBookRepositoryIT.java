f8356ec534276a7ff827d6749c0761d402a794cb
Roy <roy@codecraftr.nl>
2022-04-10 20:58:56 +0200

    Integration test

---
+++ src/test/java/nl/codecraftr/shelfhelp/csv/CsvBookRepositoryIT.java
@@ -0,0 +1,12 @@
+package nl.codecraftr.shelfhelp.csv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CsvBookRepositoryIT {
+    @Test
+    void shouldReturnBooksGivenCsvEntries() {
+        assertThat(false).isFalse();
+    }
+}
