e23a6ff6091c47d06f51d97aed3550b59cb00993
Roy <roy@codecraftr.nl>
2022-04-10 21:42:22 +0200

    Dirty way of reading csv file from classpath

--- src/main/java/nl/codecraftr/shelfhelp/csv/CsvRepository.java
+++ src/main/java/nl/codecraftr/shelfhelp/csv/CsvRepository.java
@@ -1,5 +1,8 @@
 package nl.codecraftr.shelfhelp.csv;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Set;
 import nl.codecraftr.shelfhelp.domain.model.Book;
@@ -9,7 +12,25 @@ import nl.codecraftr.shelfhelp.domain.model.Book;
  */
 public class CsvRepository {
 
+  /**
+   * Retrieve all books from file.
+   */
   public Set<Book> getAll() {
+
+    InputStream inputStream = this.getClass().getResourceAsStream("/books.csv");
+    StringBuilder resultStringBuilder = new StringBuilder();
+    try (BufferedReader br
+        = new BufferedReader(new InputStreamReader(inputStream))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        resultStringBuilder.append(line).append("\n");
+      }
+    } catch (Exception e) {
+      System.out.println("Error reading file");
+    }
+
+    System.out.println(resultStringBuilder);
+
     return Collections.emptySet();
   }
 }
