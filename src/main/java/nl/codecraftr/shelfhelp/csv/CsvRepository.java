5614f60d2e5149ed3fd39a59731451cd112d2121
Roy <roy@codecraftr.nl>
2022-04-11 08:39:04 +0200

    Extract file reading

--- src/main/java/nl/codecraftr/shelfhelp/csv/CsvRepository.java
+++ src/main/java/nl/codecraftr/shelfhelp/csv/CsvRepository.java
@@ -1,11 +1,6 @@
 package nl.codecraftr.shelfhelp.csv;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import nl.codecraftr.shelfhelp.domain.model.Book;
 
@@ -15,39 +10,20 @@ import nl.codecraftr.shelfhelp.domain.model.Book;
 public class CsvRepository {
 
   /**
-   * Retrieve all books from file.
+   * Retrieve all books from csv file.
    */
   public Set<Book> getAll() {
-    var csvContents = readCsvEntries();
-    final var headerRow = "Title,Effort,Payoff";
+    var csvContents = new FileLineReader().lines();
     csvContents
         .stream()
-        .filter(line -> !line.equals(headerRow))
+        .filter(this::isContentRow)
         .forEach(System.out::println);
 
     return Collections.emptySet();
   }
 
-  private List<String> readCsvEntries() {
-    var result = new ArrayList<String>();
-    var fileName = "/books.csv";
-
-    var inputStream = this.getClass().getResourceAsStream(fileName);
-    try (var br
-        = new BufferedReader(new InputStreamReader(inputStream))) {
-      String line;
-      while ((line = br.readLine()) != null) {
-        result.add(line);
-      }
-    } catch (Exception e) {
-      System.out.println("Error reading csv file: " + e);
-    } finally {
-      try {
-        inputStream.close();
-      } catch (IOException e) {
-        System.out.println("Error closing csv file: " + e);
-      }
-    }
-    return result;
+  private Boolean isContentRow(String line) {
+    final var headerRow = "Title,Effort,Payoff";
+    return !line.equals(headerRow);
   }
 }
